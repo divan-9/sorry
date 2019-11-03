4c3eddef22be799ffeb57c18b92245c0fee680e2
Dmitry <dmitry.sergeevich.ivanov@gmail.com>
2019-11-03 10:10:35 +0300

    Fix yml and write nuget publishing script using bash

--- nuget-push.sh
+++ nuget-push.sh
@@ -1,3 +1,11 @@
-dotnet pack -c Release -p:PackageVersion=$PACKAGE_VERSION
-sed "s/GITHUB_TOKEN/$GITHUB_TOKEN/g" nuget.config.template > nuget.config
-dotnet nuget push ./src/Sorry.Analyzers/bin/Release/Sorry.Analyzers.$PACKAGE_VERSION.nupkg --source=GPR
+
+PACKAGE_VERSION="$TRAVIS_TAG"
+
+if [[ $PACKAGE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
+    dotnet pack -c Release -p:PackageVersion=$PACKAGE_VERSION
+    sed "s/GITHUB_TOKEN/$GITHUB_TOKEN/g" nuget.config.template > nuget.config
+    dotnet nuget push ./src/Sorry.Analyzers/bin/Release/Sorry.Analyzers.$PACKAGE_VERSION.nupkg --source=GPR
+else
+    echo "Requested version is invalid: [$PACKAGE_VERSION]. Publishing nuget is skipped"
+fi
+
