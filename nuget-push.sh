d5bcddd6f32d76a53224de8d4f59fa9e8a9b871c
Dmitry <dmitry.sergeevich.ivanov@gmail.com>
2019-11-04 16:50:52 +0300

    Push to nuget.org

--- nuget-push.sh
+++ nuget-push.sh
@@ -3,10 +3,7 @@ PACKAGE_VERSION="$TRAVIS_TAG"
 
 if [[ $PACKAGE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
     dotnet pack -c Release -p:PackageVersion=$PACKAGE_VERSION
-    sed "s/GITHUB_TOKEN/$GITHUB_TOKEN/g" nuget.config.template > nuget.config
-    dotnet nuget push ./src/Sorry.Analyzers/bin/Release/Sorry.Analyzers.$PACKAGE_VERSION.nupkg --source=GPR
-    rm nuget.config
+    dotnet nuget push ./src/Sorry.Analyzers/bin/Release/Sorry.Analyzers.$PACKAGE_VERSION.nupkg -k=$NUGET_KEY -s https://api.nuget.org/v3/index.json
 else
     echo "Requested version is invalid: [$PACKAGE_VERSION]. Publishing nuget is skipped"
 fi
-
