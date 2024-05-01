d45c758400d7547b3d23012ea564476f90e55847
divan <dmitry.ivanov@retailrocket.io>
2024-05-01 09:15:44 +0300

    Add logs

--- nuget-push.sh
+++ nuget-push.sh
@@ -2,7 +2,10 @@
 PACKAGE_VERSION="$1"
 
 if [[ $PACKAGE_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
+    echo "Running pack for [$PACKAGE_VERSION]..."
     dotnet pack -c Release -p:PackageVersion=$PACKAGE_VERSION
+
+    echo "Running push for [./src/Sorry.Analyzers/bin/Release/Sorry.Analyzers.$PACKAGE_VERSION.nupkg] [$NUGET_KEY]"
     dotnet nuget push ./src/Sorry.Analyzers/bin/Release/Sorry.Analyzers.$PACKAGE_VERSION.nupkg -k=$NUGET_KEY -s https://api.nuget.org/v3/index.json
 else
     echo "Requested version is invalid: [$PACKAGE_VERSION]. Publishing nuget is skipped"
