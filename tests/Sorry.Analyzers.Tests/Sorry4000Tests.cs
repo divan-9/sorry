a59b45c60d30726609103e61f147554f0bf91f06
maksim.pashkov <maksim.pashkov@retailrocket.io>
2025-05-27 09:39:06 +0300

    Add new test for sorry4000

---
+++ tests/Sorry.Analyzers.Tests/Sorry4000Tests.cs
@@ -0,0 +1,62 @@
+namespace Sorry.Analyzers.Tests
+{
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CSharp.Testing;
+    using Microsoft.CodeAnalysis.Testing;
+    using Microsoft.CodeAnalysis.Testing.Verifiers;
+    using Xunit;
+    using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
+       Sorry.Analyzers.Sorry4000DenyPrimaryConstructorAnalyzer>;
+
+    [Trait(Spec.Trait, Spec.SP1879.UserVoice1)]
+    [Trait(Spec.Trait, Spec.SP1879.UserVoice2)]
+    [Trait(Spec.Trait, Spec.SP1879.UserVoice3)]
+    public class Sorry4000Tests
+    {
+        [Fact]
+        [Trait(Spec.Trait, Spec.SP1879.R1)]
+        public async Task T1()
+        {
+            var code = """
+                                namespace A;
+                                public class B()
+                                {
+                                }
+                                """;
+
+            var expected = Verifier.Diagnostic()
+                .WithSpan(startLine: 2, startColumn: 15, endLine: 2, endColumn: 17)
+                .WithMessage("Detected primary constructor usage");
+
+            var sut = new CSharpAnalyzerTest<Sorry4000DenyPrimaryConstructorAnalyzer, XUnitVerifier>
+            {
+                TestCode = code,
+                CompilerDiagnostics = CompilerDiagnostics.None,
+            };
+
+            sut.ExpectedDiagnostics.Add(expected);
+
+            await sut.RunAsync().ConfigureAwait(false);
+        }
+
+        [Fact]
+        [Trait(Spec.Trait, Spec.SP1879.R2)]
+        public async Task T2()
+        {
+            var code = """
+                                namespace A;
+                                public class B
+                                {
+                                }
+                                """;
+
+            var sut = new CSharpAnalyzerTest<Sorry4000DenyPrimaryConstructorAnalyzer, XUnitVerifier>
+            {
+                TestCode = code,
+                CompilerDiagnostics = CompilerDiagnostics.None,
+            };
+
+            await sut.RunAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ tests/Sorry.Analyzers.Tests/Spec.cs
@@ -0,0 +1,29 @@
+﻿namespace Sorry.Analyzers.Tests
+{
+    public static class Spec
+    {
+        public const string Trait = "spec";
+
+        public static class SP1879
+        {
+            public const string UserVoice1 = @"
+            Я, разработчик RetailRocket, хочу запретить использование primary constructor,
+            чтобы инициализация экземпляра была единообразной";
+
+            public const string UserVoice2 = @"
+            Я, разработчик RetailRocket, хочу запретить использование primary constructor,
+            чтобы упростить навигацию по кодовой базе";
+
+            public const string UserVoice3 = @"
+            Я, разработчик RetailRocket, хочу запретить использование primary constructor,
+            чтобы при чтении кода метода класса понимать идет ли речь о параметре метода
+            или о поле экземпляра";
+
+            public const string R1 = @"
+                Если в коде класса используется primary constructor, то будет ошибка анализа RRCA0001 этого кода";
+
+            public const string R2 = @"
+                Если в коде класса нет никакого конструктора, то не будет ошибки анализа этого кода";
+        }
+    }
+}
